acbbe107bb55d10561ac9299e8e9451f9d5ded66
Kevin Lamping <klamping@gmail.com>
2017-09-06 17:19:58 -0500

    initial commit

--- test/test.js
+++ test/test.js
@@ -1,7 +1,22 @@
+const expect = require('chai').expect;
+
 describe('My React Test', function () {
-  it('should find moana', function () {
+  beforeEach(function() {
     browser.url('./');
+    browser.waitForText('[data-reactroot]');
+  })
 
+  it('should find moana', function () {
+    browser.setValue('#q', 'moana');
 
+    browser.waitForText('.tt-dropdown-menu');
+
+    browser.keys('Down arrow');
+    browser.keys('Enter');
+
+    let pageHeading = browser.getText('h1');
+
+    expect(pageHeading).to.equal('MOANA');
+    expect(browser.getText('.tagline')).to.equal('The ocean is calling.');
   })
 })
